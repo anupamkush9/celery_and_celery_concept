27fd0529e079e5d684727b8bd5f61168cfbc1040
Anupamkush9 <anupam.kush9@gmail.com>
2022-11-25 18:54:15 +0530

    celery concept explained

---
+++ projectApp/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from .tasks import add
+# Create your views here.
+def Indexview(request):
+    print("--------------------------------------")
+    # print(add(45, 56).delay())  //Incorrect way of Adding task to celery.
+    add.delay(45,89)
+    print("++++++++brfore call add method++++++++++++=")
+    return HttpResponse("<h1>Hello world</h1>")
---
+++ readme.md
@@ -0,0 +1,5 @@
+### command for running viewing celery task console
+### OR 
+### command for view the console of celery
+
+$ celery -A <project_name> worker -l info
