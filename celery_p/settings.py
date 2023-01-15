13d45bebdd0392e43a9ab9341639099449e3e9d7
Anupamkush9 <anupam.kush9@gmail.com>
2023-01-15 22:55:22 +0530

    docker and docker-compose.yml file added

--- celery_p/settings.py
+++ celery_p/settings.py
@@ -121,8 +121,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # Celery Configuration Options
-CELERY_BROKER_URL = 'redis://127.0.0.1:6379'
-CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379'
+CELERY_BROKER_URL = 'redis://cache:6377'
+# CELERY_BROKER_URL = 'redis://localhost:6379'
+CELERY_RESULT_BACKEND = 'redis://cache:6377'
+# CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
