1a4629ee7ad2ced62f36a483b8527552251dba02
Anupamkush9 <anupam.kush9@gmail.com>
2023-05-07 22:28:11 +0530

    race condition

--- projectApp/views.py
+++ projectApp/views.py
@@ -1,6 +1,10 @@
+import time
 from django.shortcuts import render
 from django.http import HttpResponse
-from .tasks import add, mul
+from .tasks import add, mul, update_user
+from .models import Customer
+
+
 # Create your views here.
 def Indexview(request):
     print("--------------------------------------")
@@ -9,3 +13,11 @@ def Indexview(request):
     print("mul::",mul.delay(45,89))
     print("++++++++brfore call add method++++++++++++=")
     return HttpResponse("<h1>Hello world</h1>")
+
+def race_condition_view(request):
+    customer = Customer.objects.all()[0]
+    customer.balance = 5
+    update_user.delay()
+    time.sleep(13)
+    customer.save()
+    return HttpResponse("<h1>Hello world from race_condition_view</h1>")
