cee41f2dc029f6b46f4ed4b9a4a2315396f1cb85
Ubuntu <ubuntu@ip-172-31-1-57.ap-south-1.compute.internal>
2023-05-28 05:17:12 +0000

    allowed hosts added

--- celery_p/settings.py
+++ celery_p/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = key
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*','localhost', '127.0.0.1', '13.233.152.206']
 
 
 # Application definition
